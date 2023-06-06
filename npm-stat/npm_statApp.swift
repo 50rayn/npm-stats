8148928d23f94f6ab00a2bd997fcd3d50bff65c5
Sorin Gitlan <sgitlan@noction.com>
2023-06-06 22:20:31 +0300

    Initial Commit

---
+++ npm-stat/npm_statApp.swift
@@ -0,0 +1,17 @@
+//
+//  npm_statApp.swift
+//  npm-stat
+//
+//  Created by Sorin Gitlan on 06.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct npm_statApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
